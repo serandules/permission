04a62619f3041c923e793ad1a1a6d6705e3445f3
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-04-24 22:41:39 +0530

    updating

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-var tree = {
-    autos: {
-        '123456': {
-            '': ['read', 'write'],
-            'comments': {
-                '': ['read'],
-                '*': {
-                    '': []
-                },
-                '0001': {
-                    '': ['read', 'update'],
-                    '*': ['read'],
-                    'abcdef': {
-                        '': ['update']
-                    }
-                }
-            }
-        },
-        '*': {
-            '': ['read']
-        }
-    }
-};
-
 var has = function (tree, perms, action) {
     var allowed;
     if (!perms.length) {
@@ -123,38 +99,3 @@ var merge = function () {
 };
 
 module.exports.merge = merge;
-
-// autos:1234
-// autos:1234:*
-// autos:*
-// autos
-// autos:*:comments
-// autos:*:comments:*
-// autos:*:comments:1234
-
-/*
- console.log(can(tree, 'autos', 'read'));
- console.log(can(tree, 'autos', 'write'));
- console.log(can(tree, 'autos:123456', 'read'));
- console.log(can(tree, 'autos:123456:comments:0001:abcdef', 'update1'));
- console.log(can(tree, 'autos:0', 'read'));
- console.log(can(tree, 'autos:*', 'read'));
- console.log(can(tree, 'autos:0:comments', 'read'));
-
- var perms = {};
- allow(perms, 'autos', 'read');
- allow(perms, 'autos', 'write');
- allow(perms, 'autos:123456', 'read');
- allow(perms, 'autos:123456:comments:0001:abcdef', 'update1');
- allow(perms, 'autos:0', 'read');
- allow(perms, 'autos:*', 'read');
- allow(perms, 'autos:0:comments', 'read');
-
- console.log(can(perms, 'autos', 'read'));
- console.log(can(perms, 'autos', 'write'));
- console.log(can(perms, 'autos:123456', 'read'));
- console.log(can(perms, 'autos:123456:comments:0001:abcdef', 'update1'));
- console.log(can(perms, 'autos:0', 'read'));
- console.log(can(perms, 'autos:*', 'read'));
- console.log(can(perms, 'autos:0:comments', 'read'));
- */
